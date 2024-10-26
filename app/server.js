81c09ac0304179f48eb67145f17ce125e29afc69
Guranant Virdee <anantvirdee@gmail.com>
2024-10-26 13:14:40 -0500

    Project cloned from Techworld from Nana
    Cloned from https://gitlab.com/twn-devops-bootcamp/latest/07-docker/js-app.git
    Modified server.js to remove localhost from the connection string

--- app/server.js
+++ app/server.js
@@ -21,7 +21,8 @@ app.get('/profile-picture', function (req, res) {
 });
 
 // use when starting application locally with node command
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+//let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+let mongoUrl = "mongodb://admin:password@mongodb:27017";
 
 // use when starting application as docker container, part of docker-compose
 let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
@@ -36,7 +37,7 @@ let collectionName = "users";
 app.get('/get-profile', function (req, res) {
   let response = {};
   // Connect to the db using local application or docker compose variable in connection properties
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
@@ -57,7 +58,7 @@ app.get('/get-profile', function (req, res) {
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
   // Connect to the db using local application or docker compose variable in connection properties
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
